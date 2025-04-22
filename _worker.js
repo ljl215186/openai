47cceb2a2439abd307a4c82a478e808266e2a9b6
ljl215186 <291058575@qq.com>
2025-04-22 13:10:18 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,10 +1,21 @@
 export default {
   async fetch(request, env) {
-    const _url = new URL(request.url);
-    _url.hostname = _url.pathname.startsWith("/gh/")
-      ? "cdn.jsdelivr.net"
-      : "www.baidu.com";
-    const req = new Request(_url, request);
-    return fetch(req);
+    const TELEGRAPH_URL = 'https://api.openai.com';
+    const url = new URL(request.url);
+    url.host = TELEGRAPH_URL.replace(/^https?:\/\//, '');
+    
+    const modifiedRequest = new Request(url.toString(), {
+      headers: request.headers,
+      method: request.method,
+      body: request.body,
+      redirect: 'follow'
+    });
+  
+    const response = await fetch(modifiedRequest);
+    const modifiedResponse = new Response(response.body, response);
+  
+    // 添加允许跨域访问的响应头
+    modifiedResponse.headers.set('Access-Control-Allow-Origin', '*');
+    return modifiedResponse;
   },
 };
